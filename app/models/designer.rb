9d8813390bb60396ad23baaa64494024308fc382
Maureen Ndereba <117657621+maureen-ndereba@users.noreply.github.com>
2023-07-30 13:30:47 +0300

    Update designer model

--- app/models/designer.rb
+++ app/models/designer.rb
@@ -1,6 +1,8 @@
+
 class Designer < ActiveRecord::Base
     # Define the one-to-many association between Designer and ProjectProposal
     has_many :project_proposals
+    has_secure_password
   
     # Mount the AvatarUploader to the :avatar column
   mount_uploader :avatar, AvatarUploader
