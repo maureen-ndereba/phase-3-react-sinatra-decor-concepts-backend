4b0461cb9d0238ddb992d83d6b5b533f1070f827
Maureen Ndereba <117657621+maureen-ndereba@users.noreply.github.com>
2023-07-30 13:31:13 +0300

    Edit seed data to json

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,115 +1,147 @@
 puts "🌱 Seeding spices..."
+require 'json'
+
 # Seed data for designers
-designer1 = Designer.create(name: 'Lakisha Njoki', email: 'lakisha@example.com', password: 'password123')
-designer2 = Designer.create(name: 'Monique Atieno', email: 'monique@example.com', password: 'password456')
-designer3 = Designer.create(name: 'Luna Nandi', email: 'luna@example.com', password: 'password789')
-designer4 = Designer.create(name: 'Samuel Owiro', email: 'samuel@example.com', password: 'password1011')
-designer5 = Designer.create(name: 'Jeffrey Fauzia', email: 'jeffrey@example.com', password: 'password1213')
+designers_data = [
+    {
+      name: 'Lakisha Njoki',
+      email: 'lakisha@example.com',
+      password: 'password123'
+    },
+    {
+      name: 'Samuel Owiro',
+      email: 'samuel@example.com',
+      password: 'password1011'
+    },
+    {
+      name: 'Jeffrey Fauzia',
+      email: 'jeffrey@example.com',
+      password: 'password1213'
+    }
+  ]
+
+# Convert designers_data to JSON and store it as a string
+designers_json = JSON.generate(designers_data)
+puts designers_json
+
+# Parse designers_json back to a Ruby hash
+designers_hash = JSON.parse(designers_json)
 
 # Seed data for project proposals
-project_proposal1 = designer1.project_proposals.create(
-    title: 'The Blue House',
-    description: 'A beautiful blue-themed house.',
-    time_estimate: '24 days',
-    material_list: [
-      'Blue Paint (4 ltrs)',
-      'White Paint (4 ltrs)',
-      'Paint Primer (10 ltrs)',
-      'Sandpaper (15 mtrs)',
-      'Paint Brushes (5 pcs)',
-      'Roller Brushes (3 pcs)',
-      'Drop Cloth (2 pcs)'
-    ]
-  )
-  
-  project_proposal2 = designer1.project_proposals.create(
-    title: 'The Pink Container Project',
-    description: 'A unique project using pink shipping containers.',
-    time_estimate: '30 days',
-    material_list: [
-      'Pink Paint (5 ltrs)',
-      'Shipping Containers (3 units)',
-      'Steel Beams (10 pcs)',
-      'Glass Windows (20 pcs)',
-      'Insulation Foam (5 sheets)',
-      'Metal Screws (100 pcs)',
-      'Door Handles (10 pcs)'
-    ]
-  )
-  
-  project_proposal3 = designer2.project_proposals.create(
-    title: 'Black on Black House',
-    description: 'A modern house with a black color scheme.',
-    time_estimate: '28 days',
-    material_list: [
-      'Black Paint (6 ltrs)',
-      'Gray Paint (2 ltrs)',
-      'Concrete Blocks (200 pcs)',
-      'Glass Railing (30 mtrs)',
-      'Floor Tiles (50 sqm)',
-      'LED Light Fixtures (15 pcs)',
-      'Stainless Steel Sink (1 pc)'
-    ]
-  )
-  
-  project_proposal4 = designer3.project_proposals.create(
-    title: 'Boat House',
-    description: 'A house inspired by boats and waterfront living.',
-    time_estimate: '35 days',
-    material_list: [
-      'Wooden Planks (100 pcs)',
-      'Nautical Rope (50 mtrs)',
-      'Anchor (1 pc)',
-      'Porthole Windows (10 pcs)',
-      'Marine Varnish (5 ltrs)',
-      'Boat Cleats (10 pcs)',
-      'Captain\'s Wheel (1 pc)'
-    ]
-  )
-  
-  project_proposal5 = designer4.project_proposals.create(
-    title: 'Modern Minimalist Apartment',
-    description: 'An apartment with a minimalistic design.',
-    time_estimate: '20 days',
-    material_list: [
-      'Neutral Paint (3 ltrs)',
-      'Marble Flooring (50 sqm)',
-      'Minimalist Furniture Set (1 set)',
-      'Wall Art (5 pcs)',
-      'Floor Lamps (2 pcs)',
-      'Area Rugs (3 pcs)',
-      'Curtains (10 pcs)'
-    ]
-  )
-  
-  project_proposal6 = designer5.project_proposals.create(
-    title: 'Garden Retreat',
-    description: 'A cozy garden retreat with a rustic charm.',
-    time_estimate: '25 days',
-    material_list: [
-      'Wooden Pergola (1 pc)',
-      'Outdoor Seating Set (1 set)',
-      'Flower Pots (10 pcs)',
-      'Garden Lighting (20 pcs)',
-      'Garden Statues (5 pcs)',
-      'Fountain (1 pc)',
-      'Garden Hose (1 pc)'
-    ]
-  )
-  
+project_proposals_data = [
+    {
+      designer_id: 1,
+      title: 'The Blue House',
+      description: 'A beautiful blue-themed house.',
+      time_estimate: '24 days',
+      material_list: [
+        'Blue Paint (4 ltrs)',
+        'White Paint (4 ltrs)',
+        'Paint Primer (10 ltrs)',
+        'Sandpaper (15 mtrs)',
+        'Paint Brushes (5 pcs)',
+        'Roller Brushes (3 pcs)',
+        'Drop Cloth (2 pcs)'
+      ]
+    },
+    {
+      designer_id: 1,
+      title: 'The Pink Container Project',
+      description: 'A unique project using pink shipping containers.',
+      time_estimate: '30 days',
+      material_list: [
+        'Pink Paint (5 ltrs)',
+        'Shipping Containers (3 units)',
+        'Steel Beams (10 pcs)',
+        'Glass Windows (20 pcs)',
+        'Insulation Foam (5 sheets)',
+        'Metal Screws (100 pcs)',
+        'Door Handles (10 pcs)'
+      ]
+    },
+    {
+      designer_id: 2,
+      title: 'Black on Black House',
+      description: 'A modern house with a black color scheme.',
+      time_estimate: '28 days',
+      material_list: [
+        'Black Paint (6 ltrs)',
+        'Gray Paint (2 ltrs)',
+        'Concrete Blocks (200 pcs)',
+        'Glass Railing (30 mtrs)',
+        'Floor Tiles (50 sqm)',
+        'LED Light Fixtures (15 pcs)',
+        'Stainless Steel Sink (1 pc)'
+      ]
+    },
+    {
+      designer_id: 3,
+      title: 'Boat House',
+      description: 'A house inspired by boats and waterfront living.',
+      time_estimate: '35 days',
+      material_list: [
+        'Wooden Planks (100 pcs)',
+        'Nautical Rope (50 mtrs)',
+        'Anchor (1 pc)',
+        'Porthole Windows (10 pcs)',
+        'Marine Varnish (5 ltrs)',
+        'Boat Cleats (10 pcs)',
+        'Captain\'s Wheel (1 pc)'
+      ]
+    }
+  ]
+
+# Convert project_proposals_data to JSON and store it as a string
+project_proposals_json = JSON.generate(project_proposals_data)
+puts project_proposals_json
+
+# Parse project_proposals_json back to a Ruby hash
+project_proposals_hash = JSON.parse(project_proposals_json)
 
 # Seed data for notes
-note1 = project_proposal2.notes.create(author: designer3.name, message: 'I love the concept of using shipping containers!')
-note2 = project_proposal2.notes.create(author: designer4.name, message: 'The pink color really stands out!')
-note3 = project_proposal3.notes.create(author: designer1.name, message: 'The black-on-black design is so elegant!')
-note4 = project_proposal3.notes.create(author: designer5.name, message: 'Great use of contrasting materials.')
-note5 = project_proposal4.notes.create(author: designer2.name, message: 'The boat-themed design is very creative.')
-note6 = project_proposal4.notes.create(author: designer1.name, message: 'I like the large windows for natural light.')
-note7 = project_proposal4.notes.create(author: designer5.name, message: 'The waterfront location is perfect for this design.')
-note8 = project_proposal5.notes.create(author: designer2.name, message: 'Clean lines and simplicity - love it!')
-note9 = project_proposal5.notes.create(author: designer4.name, message: 'The use of space is well-planned.')
-note10 = project_proposal6.notes.create(author: designer3.name, message: 'The garden adds a serene touch to the design.')
-note11 = project_proposal6.notes.create(author: designer1.name, message: 'I would suggest adding more greenery.')
+notes_data = [
+    {
+      project_proposal_id: 1,
+      author: 'Samuel Owiro',
+      message: 'I love the concept of using shipping containers!'
+    },
+    {
+      project_proposal_id: 2,
+      author: 'Jeffrey Fauzia',
+      message: 'The pink color really stands out!'
+    },
+    {
+      project_proposal_id: 3,
+      author: 'Lakisha Njoki',
+      message: 'The black-on-black design is so elegant!'
+    },
+    {
+      project_proposal_id: 4,
+      author: 'Lakisha Njoki',
+      message: 'I like the large windows for natural light.'
+    },
+    {
+      project_proposal_id: 4,
+      author: 'Jeffrey Fauzia',
+      message: 'The waterfront location is perfect for this design.'
+    },
+    {
+      project_proposal_id: 5,
+      author: 'Samuel Owiro',
+      message: 'The use of space is well-planned.'
+    },
+    {
+      project_proposal_id: 6,
+      author: 'Lakisha Njoki',
+      message: 'I would suggest adding more greenery.'
+    }
+  ]
 
+# Convert notes_data to JSON and store it as a string
+notes_json = JSON.generate(notes_data)
+puts notes_json
+
+# Parse notes_json back to a Ruby hash
+notes_hash = JSON.parse(notes_json)
 
 puts "✅ Done seeding!"
